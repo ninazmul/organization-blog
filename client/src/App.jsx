ba67a99a36fcf69a142ed460172b02b1e89fd94d
ninazmul <nazmulsaw@gmail.com>
2024-04-27 12:13:28 +0600

    install react js and tailwind css and create the first templete

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl text-red-500'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
