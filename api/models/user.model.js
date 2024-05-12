b4e845dd6efa991cd3cfd3290081267816c5c09f
ninazmul <nazmulsaw@gmail.com>
2024-05-12 13:20:57 +0600

    isAdmin functionality done

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "https://i.ibb.co/NjvDZcY/user-7993222.png",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   },
   { timestamps: true }
 );
