4f499ed38acfe96925892e7b3fcfc16b7eb68e47
ninazmul <nazmulsaw@gmail.com>
2024-05-17 16:23:17 +0600

    get users api route done

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -2,9 +2,6 @@ import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 
-export const test = (req, res) => {
-  res.json({ message: "API is working well!" });
-};
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
@@ -70,7 +67,43 @@ export const deleteUser = async (req, res, next) => {
 export const signout = (req, res, next) => {
   try {
     res.clearCookie('access_token').status(200).json('User has been signed out!');
-  } catch (error){
+  } catch (error) {
     next(error);
   }
-}
+};
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You're not allowed to see all users!"));
+  }
+  try {
+    const startIndx = parseInt(req.query.startIndx) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+    const users = await User.find().sort({ createdAt: sortDirection }).skip(startIndx).limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() = 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({ users: usersWithoutPassword, totalUsers, lastMonthUsers });
+
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controllers/user.controllers.js';
+import { deleteUser, getUsers, signout, updateUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers)
 
 export default router;
