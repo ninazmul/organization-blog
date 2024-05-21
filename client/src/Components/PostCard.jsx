e01913e03dfa4b845a6278d86bcfe0dd202c8f18
ninazmul <nazmulsaw@gmail.com>
2024-05-21 16:28:42 +0600

    partial homepage done

--- client/src/Components/PostCard.jsx
+++ client/src/Components/PostCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function PostCard({ post }) {
   return (
-    <div className="group relative w-full border border-teal-500 hover:border-2 h-96 overflow-hidden rounded-lg sm:w-80 transition-all">
+    <div className="group relative w-full border border-teal-500 hover:border-2 overflow-hidden rounded-lg sm:w-80 transition-all">
       <Link to={`/post/${post.slug}`}>
         <img
           src={post.image}
