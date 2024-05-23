7418642f11096508688c3e67181f61854e7f6ae3
ninazmul <nazmulsaw@gmail.com>
2024-05-23 20:07:13 +0600

    reverse code

--- client/src/Pages/UpdatePost.jsx
+++ client/src/Pages/UpdatePost.jsx
@@ -20,34 +20,34 @@ export default function UpdatePost() {
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
-  const [publishError, setPublishError] = useState(null);
-  const { postId } = useParams();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    try {
-      const fetchPost = async () => {
-        const res = await fetch(`/api/post/getPosts?postId=${postId}`);
-        const data = await res.json();
-        if (!res.ok) {
-          setPublishError(data.message);
-          return;
+    const [publishError, setPublishError] = useState(null);
+    const { postId } = useParams();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        try {
+            const fetchPost = async () => {
+                const res = await fetch(`/api/post/getposts?postId=${postId}`);
+                const data = await res.json();
+                if (!res.ok) {
+                    setPublishError(data.message);
+                    return;
+                }
+                if (res.ok) {
+                    setPublishError(null);
+                    setFormData(data.posts[0]);
+                }
+            };
+            fetchPost();
+        } catch (error) {
+            console.log(error.message);
         }
-        if (res.ok) {
-          setPublishError(null);
-          setFormData(data.posts[0]);
-        }
-      };
-      fetchPost();
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, [postId]);
+    }, [postId]);
 
   const handleUploadImage = async () => {
     try {
       if (!file) {
-        setImageUploadError("Please select an image!");
+        setImageUploadError("Please, Select an Image!");
         return;
       }
       setImageUploadError(null);
@@ -83,10 +83,10 @@ export default function UpdatePost() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`/api/post/updatepost/${postId}`, {
+      const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
         method: "PUT",
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "Application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -100,7 +100,7 @@ export default function UpdatePost() {
         navigate(`/post/${data.slug}`);
       }
     } catch (error) {
-      setPublishError("Something went wrong!");
+      setPublishError("Something went wrong!!!");
     }
   };
 
